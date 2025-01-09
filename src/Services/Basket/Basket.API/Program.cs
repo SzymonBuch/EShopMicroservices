018ccb929febd863bb6dd4e311e4df8d10826340
SzymonBuch <szymonbuch1@gmail.com>
2025-01-09 23:01:05 +0100

    Registered services and created basket repository and BasketNotFoundException

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,18 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 var app = builder.Build();
 
+app.MapCarter();
 app.Run();
