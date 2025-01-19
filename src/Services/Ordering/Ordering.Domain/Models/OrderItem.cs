dd2ae84ac0b2bd40271d3764fbd1c92ce84f4868
SzymonBuch <szymonbuch1@gmail.com>
2025-01-19 22:56:08 +0100

    Main models implementation

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<Guid>
+    {
+        internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+        {
+            Id = orderId;
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public Guid OrderId { get; private set; } = default!;
+        public Guid ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+    }
+}
